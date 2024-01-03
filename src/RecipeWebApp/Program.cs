ddff7a24d6387a0e2b9510fd3d6d49bcfd9f7d59
Alexander Katkov <afargoth@gmail.com>
2024-01-03 17:44:04 +0700

    Use startup.cs
    Add an empty Index razor page

--- src/RecipeWebApp/Program.cs
+++ src/RecipeWebApp/Program.cs
@@ -4,12 +4,14 @@ namespace RecipeWebApp
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ src/RecipeWebApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace RecipeWebApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
