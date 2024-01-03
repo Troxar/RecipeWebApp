4eec60e77fb2912a99eb80f7609c5b92e9a0a1ed
Alexander Katkov <afargoth@gmail.com>
2024-01-03 19:21:49 +0700

    Show recipe list on Index page

--- src/RecipeWebApp/Pages/Index.cshtml.cs
+++ src/RecipeWebApp/Pages/Index.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeWebApp.ViewModels;
 
 namespace RecipeWebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<RecipeSummaryViewModel> Recipes { get; private set; }
+
         public void OnGet()
         {
+            Recipes = Array.Empty<RecipeSummaryViewModel>();
         }
     }
 }
---
+++ src/RecipeWebApp/ViewModels/RecipeSummaryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeWebApp.ViewModels
+{
+    public class RecipeSummaryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TimeToCook { get; set; }
+    }
+}
