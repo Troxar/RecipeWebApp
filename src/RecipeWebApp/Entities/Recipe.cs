de07f42ff8dc5a9bf950296684f11c5c1a4c2394
Alexander Katkov <afargoth@gmail.com>
2024-01-04 13:05:17 +0700

    Add extra fields to recipe
    Add extra recipe fields migration

--- src/RecipeWebApp/Entities/Recipe.cs
+++ src/RecipeWebApp/Entities/Recipe.cs
@@ -7,7 +7,8 @@
         public TimeSpan TimeToCook { get; set; }
         public bool IsDeleted { get; set; }
         public string Method { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool IsVegan { get; set; }
         public ICollection<Ingredient> Ingredients { get; set; }
-
     }
 }
