c695411a62aab1641981dedf9fd16e2b4cb77ee4
Alexander Katkov <afargoth@gmail.com>
2024-02-13 12:15:48 +0700

    Use Serilog

---
+++ src/RecipeWebApp/Configs/FileLoggingOptions.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace RecipeWebApp.Configs
+{
+    public class FileLoggingOptions
+    {
+        public bool Enable { get; set; }
+        public string Path { get; set; }
+        public RollingInterval RollingInterval { get; set; }
+        public string OutputTemplate { get; set; }
+        public long FileSizeLimitBytes { get; set; }
+        public int RetainedFileCountLimit { get; set; }
+    }
+}
--- src/RecipeWebApp/Program.cs
+++ src/RecipeWebApp/Program.cs
@@ -1,19 +1,59 @@
+using RecipeWebApp.Configs;
+using Serilog;
+
 namespace RecipeWebApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog(ConfigureSerilog)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseIIS();
                     webBuilder.UseIISIntegration();
                 });
+
+        private static void ConfigureSerilog(HostBuilderContext context, LoggerConfiguration configuration)
+        {
+            configuration.ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+
+            var loggingOptions = context.Configuration
+                .GetSection("FileLogging").Get<FileLoggingOptions>();
+
+            if (loggingOptions?.Enable == true)
+            {
+                configuration.WriteTo.File(
+                    path: loggingOptions.Path,
+                    rollingInterval: loggingOptions.RollingInterval,
+                    outputTemplate: loggingOptions.OutputTemplate,
+                    fileSizeLimitBytes: loggingOptions.FileSizeLimitBytes,
+                    retainedFileCountLimit: loggingOptions.RetainedFileCountLimit
+                );
+            }
+        }
     }
 }
